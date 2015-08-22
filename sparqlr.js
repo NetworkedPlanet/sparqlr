371620cf4573f32bd1c33f8f97b93f102852fa44
Florian Rathgeber <florian.rathgeber@gmail.com>
2015-08-22 16:07:07 +0100

    Remove TODO leftovers

--- sparqlr.js
+++ sparqlr.js
@@ -90,31 +90,6 @@ Meteor.methods({
         var chart = Charts.findOne(chartId);
         Charts.update(chartId, { set: { opts: opts }});
     },
-
-  deleteTask: function (taskId) {
-    var task = Tasks.findOne(taskId);
-    if (task.private && task.owner !== Meteor.userId()) {
-      // If the task is private, make sure only the owner can delete it
-      throw new Meteor.Error("not-authorized");
-    }
-    Tasks.remove(taskId);
-  },
-  setChecked: function (taskId, setChecked) {
-    var task = Tasks.findOne(taskId);
-    if (task.private && task.owner !== Meteor.userId()) {
-      // If the task is private, make sure only the owner can check it off
-      throw new Meteor.Error("not-authorized");
-    }
-    Tasks.update(taskId, { $set: { checked: setChecked} });
-  },
-  setPrivate: function (taskId, setToPrivate) {
-    var task = Tasks.findOne(taskId);
-    // Make sure only the task owner can make a task private
-    if (task.owner !== Meteor.userId()) {
-      throw new Meteor.Error("not-authorized");
-    }
-    Tasks.update(taskId, { $set: { private: setToPrivate } });
-  }
 });
 
 Router.map(function(){
