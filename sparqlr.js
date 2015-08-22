f49647ebd413794dbe7dfb04d3eb71fd4fde54ca
Joseph David Borġ <joe@josephb.org>
2015-08-22 13:07:33 +0100

    Added iron routing and added come pages to route to

--- sparqlr.js
+++ sparqlr.js
@@ -1,86 +1,27 @@
-Tasks = new Mongo.Collection("tasks");
+Charts = new Mongo.Collection("charts");
 
 if (Meteor.isServer) {
-  Meteor.publish("tasks", function () {
-    return Tasks.find({
-      $or: [
-        { private: {$ne: true} },
-        { owner: this.userId }
-      ]
-    });
-  });
+
 }
 
 if (Meteor.isClient) {
-  // This code only runs on the client
-  Meteor.subscribe("tasks");
-  Template.body.helpers({
-    tasks: function () {
-      if (Session.get("hideCompleted")) {
-        // If hide completed is checked, filter tasks
-        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
-      } else {
-        // Otherwise, return all of the tasks
-        return Tasks.find({}, {sort: {createdAt: -1}});
-      }
-},
-    hideCompleted: function () {
-      return Session.get("hideCompleted");
-},
-    incompleteCount: function () {
-      return Tasks.find({checked: {$ne: true}}).count();
+  Template.addChart.events({
+    "submit .newChart": function (event) {
+      event.preventDefault();
+      var dataSource = event.target.dataSource.value;
+      Meteor.call("addChart", dataSource);
     }
   });
-  Template.body.events({
-  "submit .new-task": function (event) {
-    // Prevent default browser form submit
-    event.preventDefault();
-
-    // Get value from form element
-    var text = event.target.text.value;
-
-    // Insert a task into the collection
-    Meteor.call("addTask", text);
-
-    // Clear form
-    event.target.text.value = "";
-
-  },
-  "change .hide-completed input": function (event) {
-    Session.set("hideCompleted", event.target.checked);
-  }
-});
-  Template.task.helpers({
-    isOwner: function () {
-      return this.owner === Meteor.userId();
-    }
-  });
-  Template.task.events({
-    "click .toggle-checked": function () {
-      // Set the checked property to the opposite of its current value
-      Meteor.call("setChecked", this._id, ! this.checked);
-    },
-    "click .delete": function () {
-      Meteor.call("deleteTask", this._id);
-    },
-      "click .toggle-private": function () {
-        Meteor.call("setPrivate", this._id, ! this.private);
-      }
-    });
-  Accounts.ui.config({
-    passwordSignupFields: "USERNAME_ONLY"
-});
 }
 
 Meteor.methods({
-  addTask: function (text) {
-    // Make sure the user is logged in before inserting a task
+  addChart: function (dataSource) {
     if (! Meteor.userId()) {
       throw new Meteor.Error("not-authorized");
     }
 
-    Tasks.insert({
-      text: text,
+    Charts.insert({
+      dataSource: dataSource,
       createdAt: new Date(),
       owner: Meteor.userId(),
       username: Meteor.user().username
@@ -110,4 +51,19 @@ Meteor.methods({
     }
     Tasks.update(taskId, { $set: { private: setToPrivate } });
   }
+});
+
+Router.map(function(){
+  this.route(
+    'home',
+    {path: '/', layoutTemplate: 'shell'}
+  );
+  this.route(
+    'about',
+    {path: '/about', layoutTemplate: 'shell'}
+  );
+  this.route(
+    'addChart',
+    {path: '/addChart', layoutTemplate: 'shell'}
+  );
 });
